a7e76e8022a0e3d24c214e9d085d83070a239d32
Pascal Laurin <pascal.laurin@outlook.com>
2015-01-31 15:33:35 -0500

    Some initial clean up Added GlobalAssemblyInfo.cs Put references in place (see Solution.dgml)

---
+++ DDDTalk.Application/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows;
+
+using DDDTalk.Presentation;
+
+namespace DDDTalk.Application
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var window = new MainWindow();
+            window.Show();
+        }
+    }
+}
---
+++ GlobalAssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DDDTalk.Application")]
+[assembly: AssemblyCopyright("Copyright © Pascal Laurin 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
