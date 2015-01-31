40d0f21fa756195d398cf389d3443a5c09a4e135
Pascal Laurin <pascal.laurin@outlook.com>
2015-01-31 18:00:08 -0500

    Missing file

---
+++ DDDTalk.Application/Boostrapping/ApplicationBootstrapper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+using DDDTalk.Infrastructure.DependencyInjection;
+using DDDTalk.Infrastructure.Navigation;
+
+namespace DDDTalk.Application.Boostrapping
+{
+    public class ApplicationBootstrapper
+    {
+        private IContainer container;
+
+        public void Initialize()
+        {
+            var infrastructureBootstrapper = new InfrastructureBootstrapper();
+            infrastructureBootstrapper.Initialize();
+
+            this.container = infrastructureBootstrapper.Container;
+
+            var dataBootstrapper = new DataBootstrapper(this.container);
+            dataBootstrapper.Initialize();
+
+            var presentationBootstrapper = new PresentationBootstrapper(this.container);
+            presentationBootstrapper.Initialize();
+
+            // Initialize other application services
+        }
+
+        public IMainWindow CreateMainWindow()
+        {
+            return this.container.Resolve<IMainWindow>();
+        }
+    }
+}
